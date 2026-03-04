519ab97c5a3dadb4af47fa7ba9d4b8475080b79e
Andrea Pietrobon <32714047+Piero24@users.noreply.github.com>
2026-03-04 22:47:13 +0100

    chore: update Homebrew formula and README to v1.0.1 (#42)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/brios.rb
+++ Formula/brios.rb
@@ -3,8 +3,8 @@ class Brios < Formula
 
   desc "Bluetooth Reactive Intelligent Operator for Croissant Safety"
   homepage "https://github.com/Piero24/B.R.I.O.S."
-  url "https://github.com/Piero24/B.R.I.O.S./releases/download/v1.0.0/brios-1.0.0.tar.gz"
-  sha256 "cb7c61fa6b752520db432c50e2523cf9de84926795bad86f16aece48c537a21f"
+  url "https://github.com/Piero24/B.R.I.O.S./releases/download/v1.0.1/brios-1.0.1.tar.gz"
+  sha256 "c06005ba8c982f77aacf305b0af4eb09d8d9e03ae0520c8d343c349b4a8df9b7"
   license "MIT"
 
   depends_on "python@3.12"
